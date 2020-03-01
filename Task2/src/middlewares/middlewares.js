23ed08cbd24f86dda0f13df19093d54260efd1bc
Yaroslav Kratt <yaroslav_kratt@epam.com>
2020-03-01 17:25:36 +0200

    Task 2.2: Add validation for User using Joi

--- Task2/src/middlewares/middlewares.js
+++ Task2/src/middlewares/middlewares.js
@@ -1,6 +1,26 @@
-
-
 exports.logErrors = (err, req, res, next) => {
     console.error(err.stack);
     next(err);
 };
+
+exports.validateSchema = (schema) => {
+    return (req, res, next) => {
+        const { error } = schema.validate(req.body, {
+            abortEarly: false,
+            allowUnknown: false
+        });
+        if (error !== undefined && error.isJoi) {
+            res.status(400).json(respondWithValidationError(error.details));
+        } else {
+            return  next();
+        }
+    };
+};
+
+function respondWithValidationError(schemaErrors) {
+    const errors = schemaErrors.map((error) => {
+        const { path, message } = error;
+        return { path, message };
+    });
+    return { status: 'failed', errors };
+}
--- Task2/src/routers/userRouter.js
+++ Task2/src/routers/userRouter.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
+const middleware = require('../middlewares/middlewares');
+const schemas = require('../schemas/schemas');
 
 
 router.get('/:id', userController.getUserById);
 
 router.get('/login/:login/limit/:limit', userController.getAutoSuggestUsers);
 
-router.post('/', userController.createOrUpdateUser);
+router.post('/', middleware.validateSchema(schemas.userSchema), userController.createOrUpdateUser);
 
 router.post('/delete/:id', userController.deleteUser);
 
