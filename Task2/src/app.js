d66c51a9bc6eca573e70be7ab5f93059b480adae
Yaroslav Kratt <yaroslav_kratt@epam.com>
2020-03-15 16:58:12 +0200

    Task 4.2: Add cors

--- Task2/src/app.js
+++ Task2/src/app.js
@@ -5,8 +5,11 @@ const homeRouter = require('./routers/homeRouter');
 const userRouter = require('./routers/userRouter');
 const middleware = require('./middlewares/middlewares');
 const winston = require('./logger');
+const cors = require('cors');
+
 
 app.use(express.json());
+app.use(cors());
 
 app.use('/', homeRouter);
 app.use('/users', userRouter);
